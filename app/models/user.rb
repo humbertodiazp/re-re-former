4e3d37b95579c368bd71240831b4ab132fe47706
Humberto <hdkaratechop@gmail.com>
2023-06-28 19:43:41 -0700

    add validations for username, email, & password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    validates :username, :email, :password, presence: true
+    validates :username, length: {maximum:20}
+    validates :password, length: { in: 6..20 }
+    validates :username, 
+        uniqueness: {
+            # object = user object being validated
+            # data = { model: "User", attribute: "Username", value: <username> }
+            message: ->(object, data) do
+            "Hey #{object.name}, #{data[:value]} is already taken."
+            end
+            }
+    validates :email, 
+        uniqueness: {
+            # object = user object being validated
+            # data = { model: "User", attribute: "Email", value: <email> }
+            message: ->(object, data) do
+            "That #{object.name}, #{data[:value]} is related to another account."
+            end
+            }
 end
