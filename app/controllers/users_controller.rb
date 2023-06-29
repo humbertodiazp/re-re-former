1ffd18e363986ca86c45043cd0d5df7d1879f310
Humberto <hdkaratechop@gmail.com>
2023-06-28 21:04:05 -0700

    Update new and create methods/def user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
     def def new
-        
+        @User = User.new
     end
     
     def def create
- 
+        @User = User.new(params[:user_params])
+        if @User.save
+          flash[:success] = "Object successfully created"
+          redirect_to @User
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
     
 end
