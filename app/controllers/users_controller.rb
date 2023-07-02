e28edf601f3edbb22a5aba2057f250e6371be200
Humberto <hdkaratechop@gmail.com>
2023-07-01 23:01:29 -0700

    fix typos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    
-    def create
-        @user = User.new(params[:user_params])
-        if @user.save
-          flash[:success] = "user successfully created"
-          redirect_to @user
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-    
 
-    def def update
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to @user
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+
+    def update
         @user = user.find(params[:id])
         if @user.update_attributes(params[:user])
           flash[:success] = "user was successfully updated"
@@ -26,11 +25,8 @@ class UsersController < ApplicationController
         end
     end
     
-
-    private
-
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password)
     end
-    
+
 end
